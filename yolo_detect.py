fdc18ad458f199380c2ea7ca1e8527f9df5b2053
Evan <evan.juras@gmail.com>
2024-11-29 14:36:58 -0700

    default to yolov8s.pt if model file is not found

--- yolo_detect.py
+++ yolo_detect.py
@@ -32,8 +32,8 @@ user_res = args.resolution
 
 # Check if model file exists and is valid
 if (not os.path.exists(model_path)) or (not ('.pt' in model_path)):
-    print('Model path is invalid or model was not found, please enter the path to a YOLO .pt file.')
-    sys.exit(0)
+    print('WARNING: Model path is invalid or model was not found. Using default yolov8s.pt model instead.')
+    model_path = 'yolov8s.pt'
 
 # Load the model into memory and get labemap
 model = YOLO(model_path)
