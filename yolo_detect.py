a62035e0dfb637169e1210ce52a5a19537040e74
Evan <evan.juras@gmail.com>
2024-11-29 14:09:52 -0700

    Add inference from multiple source types

--- yolo_detect.py
+++ yolo_detect.py
@@ -1,58 +1,173 @@
+import os
+import sys
+import argparse
 import cv2
+import glob
 from ultralytics import YOLO
 
+# Define and parse user input arguments
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', help='Path to YOLO model file (example: "runs/detect/train/weights/best.pt")',
+                    required=True)
+parser.add_argument('--source', help='Image source, can be image file ("test.jpg"), \
+                    image folder ("test_dir"), video file ("testvid.mp4"), or index of USB camera ("usb0")', 
+                    required=True)
+parser.add_argument('--thresh', help='Minimum confidence threshold for displaying detected objects (example: "0.4")',
+                    default=0.5)
+parser.add_argument('--resolution', help='Resolution in WxH to display inference results at (example: "640x480"), \
+                    otherwise, match source resolution',
+                    default=None)
+
+args = parser.parse_args()
+
+
+# Parse user inputs
+model_path = args.model
+img_source = args.source
+min_thresh = args.thresh
+user_res = args.resolution
+
+# Check if model file exists and is valid
+if (not os.path.exists(model_path)) or (not ('.pt' in model_path)):
+    print('Model path is invalid or model was not found, please enter the path to a YOLO .pt file.')
+    sys.exit(0)
+
 # Load the model into memory and get labemap
-model = YOLO('/content/model.pt')
+model = YOLO(model_path)
 labels = model.names
 
-# Load an image
-img_path = 'test.jpg'
-img = cv2.imread(img_path)
+# Parse input to determine if image source is a file, folder, video, or USB camera
+if os.path.isdir(img_source):
+    source_type = 'folder'
+elif os.path.isfile(img_source):
+    _, ext = os.path.splitext(img_source)
+    img_ext_list = ['.jpg','.JPG','.jpeg','.JPEG','.png','.PNG','.bmp','.BMP']
+    vid_ext_list = ['.avi','.mov','.mp4','.mkv','.wmv']
+    if ext in img_ext_list:
+        source_type = 'image'
+    elif ext in vid_ext_list:
+        source_type = 'video'
+    else:
+        print(f'File extension {ext} is not supported.')
+        sys.exit(0)
+elif 'usb' in img_source:
+    source_type = 'usb'
+    usb_idx = int(img_source[3:])
+else:
+    print(f'Input {img_source} is invalid. Please try again.')
+    sys.exit(0)
 
-# Run inference on image
-results = model(img)
+# Parse user-specified display resolution
+resize = False
+if user_res:
+    resize = True
+    resW, resH = int(user_res.split('x')[0]), int(user_res.split('x')[1])
 
-# Extract results
-detections = results[0].boxes
+# Load or initialize image source
+if source_type == 'image':
+    imgs_list = [img_source]
+elif source_type == 'folder':
+    imgs_list = []
+    filelist = glob.glob(img_source + '/*')
+    for file in filelist:
+        _, file_ext = os.path.splitext(file)
+        if file_ext in img_ext_list:
+            imgs_list.append(file)
+elif source_type == 'video': #TODO: Add setting of resolution?
+    cap = cv2.VideoCapture(source_type)
+elif source_type == 'usb': #TODO: Add setting of resolution?
+    cap = cv2.VideoCapture(usb_idx)
 
-# Initialize variable for basic object counting example
-object_count = 0
+# Initialize control and status variables
+frame_rate_calc = 0
+frame_count = 0
 
-# Go through each detection and get bbox coords, confidence, and class
-for i in range(len(detections)):
+# Begin inference loop
+while True:
 
-    # Get bounding box coordinates
-    # Ultralytics returns results in Tensor format, which have to be converted to a regular Python array
-    xyxy_tensor = detections[i].xyxy.cpu() # Detections in Tensor format in CPU memory
-    xyxy = xyxy_tensor.numpy().squeeze() # Convert tensors to Numpy array
-    xmin, ymin, xmax, ymax = xyxy.astype(int) # Extract individual coordinates and convert to int
+    # Load frame from image source
+    if source_type == 'image' or source_type == 'folder': # If source is image or image folder, load the image using its filename
+        if frame_count >= len(imgs_list):
+            print('All images have been processed. Exiting program.')
+            sys.exit(0)
+        img_filename = imgs_list[frame_count]
+        frame = cv2.imread(img_filename)
+    
+    elif source_type == 'video': # If source is a video, load next frame from video file
+        ret, frame = cap.read()
+        if not ret:
+            print('Reached end of the video file. Exiting program.')
+            break
+    
+    elif source_type == 'usb': # If source is a USB camera, grab frame from camera
+        ret, frame = cap.read()
+        if (frame is None) or (not ret):
+            print('Unable to read frames from the camera. This indicates the camera is disconnected or not working. Exiting program.')
+            break
 
-    # Get bounding box class ID and name
-    classidx = int(detections[i].cls.item())
-    classname = labels[classidx]
+    # Resize frame to desired display resolution (note: inference stage will resize frame again to the input resolution of the YOLO model)
+    if resize == True:
+        frame = cv2.resize(frame,(resW,resH))
 
-    # Get bounding box confidence
-    conf = detections[i].conf.item()
+    # Run inference on frame
+    results = model(frame, verbose=False)
 
-    # Draw box if confidence threshold is high enough
-    if conf > 0.5:
+    # Extract results
+    detections = results[0].boxes
 
-      cv2.rectangle(img, (xmin,ymin), (xmax,ymax), (0,255,0), 2)
+    # Initialize variable for basic object counting example
+    object_count = 0
 
-      label = f'{classname}: {conf*100:.2f}%'
-      labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1) # Get font size
-      label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
-      cv2.rectangle(img, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255,255,255), cv2.FILLED) # Draw white box to put label text in
-      cv2.putText(img, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1) # Draw label text
+    # Go through each detection and get bbox coords, confidence, and class
+    for i in range(len(detections)):
 
-      # Basic example: count the number of objects in the image that match the first class in the labelmap
-      if classname == labels[0]:
-        object_count = object_count + 1
+        # Get bounding box coordinates
+        # Ultralytics returns results in Tensor format, which have to be converted to a regular Python array
+        xyxy_tensor = detections[i].xyxy.cpu() # Detections in Tensor format in CPU memory
+        xyxy = xyxy_tensor.numpy().squeeze() # Convert tensors to Numpy array
+        xmin, ymin, xmax, ymax = xyxy.astype(int) # Extract individual coordinates and convert to int
 
-# Display detection results
-cv2.putText(img, f'Number of {labels[0]}s: {object_count}', (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2) # Draw total number of detected quarters in top left corner of image
-cv2.imshow('YOLO detection results',img) # Display image
+        # Get bounding box class ID and name
+        classidx = int(detections[i].cls.item())
+        classname = labels[classidx]
 
-# Wait for keypress then close all windows
-cv2.waitKey()
+        # Get bounding box confidence
+        conf = detections[i].conf.item()
+
+        # Draw box if confidence threshold is high enough
+        if conf > 0.5:
+
+            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (0,255,0), 2)
+
+            label = f'{classname}: {conf*100:.2f}%'
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1) # Get font size
+            label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255,255,255), cv2.FILLED) # Draw white box to put label text in
+            cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1) # Draw label text
+
+        # Basic example: count the number of objects in the image that match the first class in the labelmap
+        if classname == labels[0]:
+            object_count = object_count + 1
+
+    # Display detection results
+    cv2.putText(frame, f'Number of {labels[0]}s: {object_count}', (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2) # Draw total number of detected quarters in top left corner of image
+    cv2.imshow('YOLO detection results',frame) # Display image
+
+    # If inferencing on individual images, wait for user keypress before moving to next image. Otherwise, wait 5ms before moving to next frame.
+    if source_type == 'image' or source_type == 'folder':
+        key = cv2.waitKey()
+    elif source_type == 'video' or source_type == 'usb':
+        key = cv2.waitKey(5)
+    
+    if key == ord('q') or key == ord('Q'): # Press 'q' to quit
+        break
+    elif key == ord('s') or key == ord('S'): # Press 's' to pause inference
+        cv2.waitKey()
+    elif key == ord('p') or key == ord('P'): # Press 'p' to save a picture of results on this frame
+        cv2.imwrite('capture.png',frame)
+
+# Clean up
+if cap:
+    cap.release()
 cv2.destroyAllWindows()
